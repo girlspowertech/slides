215c1a93534a40a3fc167a5bf98473db7dec6e16
anig1scur <anig1scur@users.noreply.github.com>
2024-07-31 11:04:48 +0000

    deploy: 570d08509628c2e5c822626bfe6abd68baab6d64

---
+++ assets/slidev/default-DJSZ7g8m.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-ji9cbFf1.js";import{_ as r}from"../index-k8LGHcwe.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
