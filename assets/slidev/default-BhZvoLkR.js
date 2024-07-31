b19b2dbe5b9a10a9ff741699b0e9c2aeb44f586d
anig1scur <anig1scur@users.noreply.github.com>
2024-07-31 19:01:38 +0000

    deploy: eea09111469f001c724eb47639525e59da0aefe0

--- assets/slidev/default-BhZvoLkR.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-BosPWM6Z.js";import{_ as r}from"../index-DBbU5APd.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
